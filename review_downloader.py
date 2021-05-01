d674e13e532bb64f52d6ac4955c36dedbed4cec4
shadov <kuznja16@gmail.com>
2021-05-01 10:58:38 +0200

    rebuild code

--- review_downloader.py
+++ review_downloader.py
@@ -1,11 +1,11 @@
 # Kuznik Jakub
 # KNOT - product_reviews_in_czech
 # xkuzni04@stud.fit.vutbr.cz
-# This script should get all the zbozi.cz urls
+# This script should get all the zbozi.cz reviews 
 # xkuzni04@stud.fit.vutbr.cz
 
-# Script should get through all the categories from input file: all_zbozi.cz_products_url
-# And download all the products from zbozi.cz and store them to : all_zbozi.cz_products_url
+# Every server gets throught urls in 
+# logs_and_input_files/input_files_for_each_server/
 
 
 import time
@@ -22,7 +22,6 @@ from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
-
 
 # TESTING - CHROME MODE
 ##################################
@@ -54,49 +53,18 @@ driver = webdriver.Chrome(path_to_chrome_driver, options=options)
 
 catlist = []
 
-#Download all the products from given category
-def get_product_urls(category, output_file):
-
-    try:
-        driver.get(str(category))
-    except:
-        return
-
-    time.sleep(1)
-
-    while True:
-        try:
-            time.sleep(0.1)
-            # Scroll down to last name in list
-            page = driver.find_elements_by_xpath("//a[@href]")
-            driver.execute_script("window.scrollTo(0, window.scrollY + 400)")
-            #try:
-            for elem in page:
-                filter_elem = elem.get_attribute("href")
-                if filter_elem not in catlist and "vyrobek" in str(filter_elem):
-                    catlist.append(filter_elem)
-                    print(filter_elem,";",category)
-                    output_file.write("%s;%s" %(filter_elem, category))
-                #except:
-                    #continue
-        except:
-            break
-        try:
-            time.sleep(2)
-            elm = driver.find_element_by_class_name('next')
-            time.sleep(2)
-            elm.click()
-            time.sleep(2)
-        except:
-            print("Cannot open page")
-            break
+##
+# Download reviews and store them.
+# 
+def get_review(product_url, output_file):
+    print(product_url)
     return
 
 
 # Check if output files can be open
 def output_files_open():
     try:
-        outfile = open("logs_and_input_files/all_zbozi.cz_products_url", 'a')
+        outfile = open("logs_and_input_files/input_files_for_each_server_reviews/all_zbozi.cz_reviews", 'a')
     except IOError:
         sys.stderr.write("Cannot open outfile")
         return False
@@ -105,7 +73,7 @@ def output_files_open():
 
 # Check if input file can be open
 def read_file_open(server_host_name):
-    path = "logs_and_input_files/input_files_for_each_server/"
+    path = "logs_and_input_files/input_files_for_each_server_reviews/"
     path = path + server_host_name
     print(path)
     try:
@@ -116,19 +84,9 @@ def read_file_open(server_host_name):
     return read_file
 
 
-
-# Ve ll need just url that contains word "vyrobek"
-def filter_urls(urls, substring):
-    out = []
-    for url in urls:
-        if substring in url:
-            out.append(url)
-
-    return out
-
 #Get next category from file
-def get_next_category(category_file):
-    return category_file.readline()
+def get_next_page(page_file):
+    return page_file.readline()
 
 
 def main():
@@ -141,14 +99,14 @@ def main():
     output_file = output_files_open()
 
     if read_file_open(server_host_name) == False: sys.exit(2)  # check if files for tput can be open
-    category_file = read_file_open(server_host_name) #
+    page_file = read_file_open(server_host_name) #
 
-    for i in range(2140):
-        get_product_urls(get_next_category(category_file), output_file)
+    for i in range(10):
+        get_review(get_next_page(page_file), output_file)        
 
 
     output_file.close()
-    category_file.close()
+    page_file.close()
 #    display.stop()
 
 if __name__ == '__main__':
