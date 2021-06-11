88a00c7f19bd0098f937d969b2ecdc9fb18cc4d5
shadov <kuznja16@gmail.com>
2021-06-11 17:12:13 +0200

    run script in tmux

--- run_product_download.sh
+++ run_product_download.sh
@@ -1,2 +1,4 @@
-cd /mnt/minerva1/knot/projects/product_reviews_in_czech/
-nohup python3 products_url_downloader.py 88 >log 2>log_err &
+#!/bin/bash
+tmux new-session -d -s mySession -n myWindow
+tmux send-keys -t mySession:myWindow "python3 products_url_downloader.py 88 >log 2>log_err &" Enter
+exit
